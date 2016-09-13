b3c1e80e317ce93670c78fb402f2aa2e1b3dfbea
leidian <bearwming@163.com>
2016-09-13 14:05:58 +0800

    提交。。。

--- services/backend/src/main/java/com/xmm/shoptools/backend/admin/web/TjobController.java
+++ services/backend/src/main/java/com/xmm/shoptools/backend/admin/web/TjobController.java
@@ -1,19 +1,32 @@
 package com.xmm.shoptools.backend.admin.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.xmm.shoptools.backend.service.TjobService;
+import com.xmm.shoptools.backend.vo.PageResult;
+import com.xmm.shoptools.backend.vo.TjobQuery;
+import com.xmm.shoptools.backend.vo.TjobVO;
 
 @Controller
 @Scope("prototype")
 @RequestMapping("/job")
 public class TjobController extends BaseAction {
+    
+    @Autowired
+    private TjobService tjobService;
 
-	// 后台主页
-	@RequestMapping("/index")
-	public String index() {
-		return "/admin/job";
-	}
-
-
+	//后台页
+    @RequestMapping("/index")
+    public ModelAndView index() {
+        TjobQuery query = new TjobQuery();
+        ModelAndView mav = new ModelAndView("/admin/job");
+        PageResult<TjobVO> pr = null;
+        pr = tjobService.query(query);
+        mav.addObject("pr", pr);
+        return mav;
+    }
 }
