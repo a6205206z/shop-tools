ee98db85088b4d6be9899ddfa4da4d3bd838b1b1
leidian <bearwming@163.com>
2016-09-13 18:54:48 +0800

    add..

--- services/backend/src/main/java/com/xmm/shoptools/backend/admin/web/TshopController.java
+++ services/backend/src/main/java/com/xmm/shoptools/backend/admin/web/TshopController.java
@@ -1,18 +1,33 @@
 package com.xmm.shoptools.backend.admin.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.xmm.shoptools.backend.service.TshopService;
+import com.xmm.shoptools.backend.vo.PageResult;
+import com.xmm.shoptools.backend.vo.TshopQuery;
+import com.xmm.shoptools.backend.vo.TshopVO;
 
 @Controller
 @Scope("prototype")
 @RequestMapping("/tshop")
 public class TshopController extends BaseAction {
-
-	// 后台主页
-	@RequestMapping("/index")
-	public String index() {
-		return "/admin/tshop";
-	}
-
+    
+    @Autowired
+    private TshopService tshopService;
+    
+	//后台主页
+    @RequestMapping("/index")
+    public ModelAndView index() {
+        PageResult<TshopVO> pr = null;
+        TshopQuery query = new TshopQuery();
+        query.setRows(Integer.MAX_VALUE);
+        ModelAndView mav = new ModelAndView("/admin/tshop");
+        pr = tshopService.query(query);
+        mav.addObject("pr", pr);
+        return mav;
+    }
 }
